231228e4e1bb57213f8a07d1bc5295bd6ccdb5d9
Tim-Gaede <TimGaede@gmail.com>
2019-10-01 14:45:25 -0400

    Update solution example.jl

--- solution example.jl
+++ solution example.jl
@@ -2,6 +2,6 @@ function score(x::Real, y::Real)
     dist = √(x^2 + y^2)
     if dist <=  1.0;    return 10;    end # in inner circle
     if dist <=  5.0;    return  5;    end # in middle circle
-    if dist <= 10.0;    return  1 ;   end # in outer circle
+    if dist <= 10.0;    return  1;    end # in outer circle
     return 0 # outside the circles
 end
