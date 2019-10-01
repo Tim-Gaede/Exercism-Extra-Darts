e5698a8b236d04545d65482993fd5f681a4ec33b
Tim-Gaede <TimGaede@gmail.com>
2019-10-01 14:36:03 -0400

    Update darts.jl

--- darts.jl
+++ darts.jl
@@ -1,6 +1,3 @@
-def score(x, y):
-    dist = (x**2 + y**2)**0.5
-    if dist <=  1.0:    return 10 # in inner circle
-    if dist <=  5.0:    return  5 # in middle circle
-    if dist <= 10.0:    return  1 # in outer circle
-    return 0 # outside the circles
+function score(x, y)
+     
+end
